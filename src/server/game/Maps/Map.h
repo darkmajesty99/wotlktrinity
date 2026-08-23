6f57d8dc13af952a105eb71e67ab7e87fa4ce893
darkmajesty99 <jbln2005@hotmail.com>
2026-08-23 09:23:27 +0800

    Add CustomData member to Map class

--- src/server/game/Maps/Map.h
+++ src/server/game/Maps/Map.h
@@ -33,6 +33,7 @@
 #include "SpawnData.h"
 #include "Timer.h"
 #include "Transaction.h"
+#include "DataMap.h"
 #include "UniqueTrackablePtr.h"
 #include <bitset>
 #include <list>
@@ -640,6 +641,8 @@ class TC_GAME_API Map : public GridRefManager<NGridType>
         }
 
         virtual std::string GetDebugInfo() const;
+
+        DataMap CustomData;
 
     private:
         void LoadMapAndVMap(int gx, int gy);
