9dfedaf15d65bc4fabc5d2766daea6d8ac66b3b1
darkmajesty99 <jbln2005@hotmail.com>
2026-08-23 12:50:40 +0800

    Fix typo in GetLFGDungeon function call

--- src/server/scripts/Custom/autobalance.cpp
+++ src/server/scripts/Custom/autobalance.cpp
@@ -123,7 +123,7 @@ int GetForcedNumPlayers(int creatureId) {
 }
 
 void getAreaLevel(Map* map, uint8 areaid, uint8& min, uint8& max) {
-    LFGDungeonEntry const* dungeon = GetLFGDungeon(map->GetId(), map->GetDifficultyId());
+    LFGDungeonEntry const* dungeon = GetLFGDungeon(map->GetId(), map->GetDifficultyID());
 
     if (dungeon && (map->IsDungeon() || map->IsRaid()))
     {
