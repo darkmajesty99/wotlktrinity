cd332863536a2f0011d71f1ed4153873a33938b4
darkmajesty99 <jbln2005@hotmail.com>
2026-08-23 09:59:25 +0800

    Create custom_solocraft_character_stats table

---
+++ sql/custom/characters/characters.solocraft.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `custom_solocraft_character_stats`;
+CREATE TABLE IF NOT EXISTS `custom_solocraft_character_stats` (
+  `GUID` bigint unsigned NOT NULL,
+  `Difficulty` float NOT NULL,
+  `GroupSize` int NOT NULL,
+  `SpellPower` int unsigned NOT NULL DEFAULT '0',
+  `Stats` float NOT NULL DEFAULT '100',
+  PRIMARY KEY (`GUID`)
+) ENGINE=InnoDB COLLATE='utf8mb4_general_ci';
